fa393d2f68e17120a5f9156b6960c08243cce30f
xxx <xxx@x.xx>
2025-10-18 18:59:14 +0530

    kt anddroid

--- smartcli/core/build.gradle.kts
+++ smartcli/core/build.gradle.kts
@@ -1,20 +1,30 @@
 // smartcli/core/build.gradle.kts
 plugins {
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")  // ← CHANGE to Android library
+    id("org.jetbrains.kotlin.android")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
+android {
+    namespace = "com.smartcli.core"
+    compileSdk = 34
 
-kotlin {
-    jvmToolchain(8)
+    defaultConfig {
+        minSdk = 28
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
     testImplementation("junit:junit:4.13.2")
 }
