f67fcc1751275a1c21b0e237938939dfae4caa28
xxx <xxx@x.xx>
2025-10-19 10:59:02 +0530

    fix: Add custom sourceSets and AndroidManifest to core module

--- smartcli/core/build.gradle.kts
+++ smartcli/core/build.gradle.kts
@@ -20,12 +20,27 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+sourceSets {
+        getByName("main") {
+            java.srcDirs("cli", "system")
+            res.srcDirs("res") 
+            manifest.srcFile("AndroidManifest.xml")
+
+        }
+    }
+
+
+
+
+
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
     testImplementation("junit:junit:4.13.2")
 }
 
